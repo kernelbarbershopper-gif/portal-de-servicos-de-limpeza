9c6b0bc4d87f0c9befc6d4cbc95b05690dc700a1
NexaCore Manager <gerentenexacorellc@gmail.com>
2026-06-15 00:17:28 +0000

    style: align broom to sweep directly over the portal name

--- src/components/CleaningSplash.tsx
+++ src/components/CleaningSplash.tsx
@@ -103,7 +103,7 @@ export default function CleaningSplash({ onComplete }: CleaningSplashProps) {
                 duration: 3.5, 
                 ease: "easeInOut"
               }}
-              className="relative left-[-30px] top-[-50px] scale-75 md:scale-90" // Vassoura mais para cima
+              className="relative left-[-30px] top-[0px] scale-75 md:scale-90" // Vassoura alinhada com o nome central
             >
               <svg width="180" height="300" viewBox="0 0 100 200" fill="none">
                 <rect x="46" y="0" width="8" height="140" rx="4" fill="url(#wood)" />
